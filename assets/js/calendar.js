6f0721eb63bf261bb89996ee3ba579fa9cce2889
brunomeneghesso <49211562+brunomeneghesso@users.noreply.github.com>
2019-11-22 11:30:47 -0300

    arrumei o js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://togepi-91fe9.firebaseio.com/')
 
   db.download('/', function(data) {
-
-
     context = {'return' : params['anterior']}
     coDesReplace('.access', context)
 
