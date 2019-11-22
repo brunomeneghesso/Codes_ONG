4155dd8e2b55443f999b3752a54621e359f218bb
brunomeneghesso <49211562+brunomeneghesso@users.noreply.github.com>
2019-11-22 11:17:24 -0300

    js falhando

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,1 +1,13 @@
-calendar
+document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
+  let db = coDesConnect('https://togepi-91fe9.firebaseio.com/')
+
+  db.download('/', function(data) {
+
+
+    context = {'return' : params['anterior']}
+    coDesReplace('.access', context)
+
+  })
+})
