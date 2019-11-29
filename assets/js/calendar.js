8d887f665caeba983a9d44124547a67c4e74c9f8
brunomeneghesso <49211562+brunomeneghesso@users.noreply.github.com>
2019-11-29 08:06:12 -0300

    voltei o return

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -4,45 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://togepi-91fe9.firebaseio.com/')
 
   db.download('/', function(data) {
-    if(typeof(params['anterior']) == 'undefined' ){
-        if(typeof(params['anteriores']) == 'undefined'|| params['anteriores'].length == 0){
-          context = {'return' : 'main',
-          'no_antigos':true,
-          'no_keep_return':true,
-          'e_antigos':false,
-          'keep_return':false}
-        }
-        else{
-          context = {'return' : params['anteriores'][-1],
-          'antigos':params['anteriores'],
-          'keep_return':params['anteriores'].slice(1,),
-          'no_antigos':false,
-          'no_keep_return':false,
-          'e_antigos':true,
-          'keep_return':true}
-        }
-      }
-    else{
-      if(typeof(params['anteriores']) == 'undefined' ){
-        context = {'return' : params['anterior'],
-        'antigos':[params['anterior']],
-        'no_antigos':false,
-        'no_keep_return':true,
-        'e_antigos':true,
-        'keep_return':false}
-      }
-      else{
-        let antigos = params['anteriores']
-        antigos.push(params['anterior'])
-        context = {'return' : params['anterior'],
-        'antigos':antigos,
-        'keep_return': params['anterior'],
-        'no_antigos':false,
-        'no_keep_return':true,
-        'e_antigos':true,
-        'keep_return':false}
-      }
-    }
+    context={'return':params['anterior']}
   	
     coDesReplace('.access', context)
 
