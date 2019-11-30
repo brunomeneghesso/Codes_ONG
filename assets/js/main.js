3d1188773f76abefa99106cd5dff93dacfb80ac8
brunomeneghesso <49211562+brunomeneghesso@users.noreply.github.com>
2019-11-29 23:18:41 -0300

    deveria funcionar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
   let value = params['key']
   let db = coDesConnect('https://togepi-91fe9.firebaseio.com/')
-
+  if(params['filtrado']=='sim'){
+  	let eventos='eventos-filtro'
+  	let fil='sim'
+  }
+  else{
+  	let eventos='eventos'
+  	let fil = 'nao'
+  }
   db.download('/', function(data) {
-    context={'return':params['anterior']}
+    context={'return':params['anterior'],
+			'ev':eventos
+			'filtrado':fil}
     
     coDesReplace('.access', context)
 
