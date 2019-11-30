94ff5f5fc003cf4926c36aee5f21208c79e6664d
GabrielYamashita <49691883+GabrielYamashita@users.noreply.github.com>
2019-11-29 23:59:13 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
   let value = params['key']
   let db = coDesConnect('https://togepi-91fe9.firebaseio.com/')
-  if(params['filtrado']=='sim'){
-  	let eventos='eventos-filtro'
-  	let fil='sim'
-  }
-  else{
-  	let eventos='eventos'
-  	let fil = 'nao'
-  }
+
   db.download('/', function(data) {
-    context={'return':params['anterior'],
-			'ev':eventos
-			'filtrado':fil}
+    context={'return':params['anterior']}
     
     coDesReplace('.access', context)
 
