6ba2d944ec82974ac754f7db41b6166bf321bf9e
nicole costa <nicolesarvasi@hotmail.com>
2019-12-06 18:11:56 -0300

    arrumar calendario e layout'

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@ let currentYear = today.getFullYear();
 let selectYear = document.getElementById("year");
 let selectMonth = document.getElementById("month");
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 let monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
